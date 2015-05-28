c0e800dcef88a6d2c655fc4690b87b68d126c6db
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-05-28 17:22:44 +0000

    move logging to a module to show api

--- SockBot.js
+++ SockBot.js
@@ -1,31 +1,13 @@
 'use strict';
 
 var client = require('./client');
-
+var logger = require('./modules/log2console');
 var config = {
     server: 'irc.darkmyst.org',
     nick: 'sockbot',
     channels: ['#crossings_ooc']
 };
 
-var time = () => new Date().toISOString().replace(/^[^T]+T/, '').replace(/[.].*$/, ''),
-    log = (actor, target, text) => {
-        if (!text) {
-            text = target;
-            target = '';
-        }
-        if (target) {
-            target = ' => ' + target;
-        } else {
-            target = '';
-        }
-        console.log('(' + time() + ') ' + (actor || '') + target + ': ' + text); //eslint-disable-line no-console
-    };
 
 var events = client.connect(config);
-
-events.on('message_received', (payload) => log(payload.who, payload.what, payload.text));
-events.on('message_sent', (payload) => log(payload.who, payload.what, payload.text));
-events.on('user_action', (payload) => log(payload.who, payload.what, payload.text));
-events.on('channel_action', (payload) => log(payload.who, payload.what, payload.text));
-events.on('error', (payload) => log('ERROR', '', JSON.stringify(payload.raw)));
+logger.begin(events);
---
+++ modules/log2console.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// This is a minimal module to show what the module API is.
+
+var time = () => new Date().toISOString().replace(/^[^T]+T/, '').replace(/[.].*$/, ''),
+    log = (actor, target, text) => {
+        if (!text) {
+            text = target;
+            target = '';
+        }
+        if (target) {
+            target = ' => ' + target;
+        } else {
+            target = '';
+        }
+        console.log('(' + time() + ') ' + (actor || '') + target + ': ' + text); //eslint-disable-line no-console
+    };
+
+// Called when bot starts
+exports.begin = (client) => {
+    client.on('message_received', (payload) => log(payload.who, payload.what, payload.text));
+    client.on('message_sent', (payload) => log(payload.who, payload.what, payload.text));
+    client.on('user_action', (payload) => log(payload.who, payload.what, payload.text));
+    client.on('channel_action', (payload) => log(payload.who, payload.what, payload.text));
+    client.on('error', (payload) => log('ERROR', '', JSON.stringify(payload.raw)));
+    setTimeout(() => client.notice('accalia', 'sockbot has connected'), 10000);
+};
+
+// Called when bot is exiting
+exports.stop = () => {};
