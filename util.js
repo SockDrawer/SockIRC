4b86ec1aea8c1ab00464fd645e16a38bba5e49db
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-06-02 02:22:52 +0000

    switch util to use functions instead of arrow functions

--- util.js
+++ util.js
@@ -1,16 +1,28 @@
 'use strict';
 const fs = require('fs');
-const log = (message) => console.log(message), //eslint-disable-line no-console
-    warn = (message) => console.warn(message), //eslint-disable-line no-console
-    error = (message) => console.error(message), //eslint-disable-line no-console
-    readFile = fs.readFile,
-    loadModule = (path) => require(path);
+const readFile = fs.readFile;
+
+function log(message) {
+    console.log(message); //eslint-disable-line no-console
+}
+
+function warn(message) {
+    console.warn(message); //eslint-disable-line no-console
+}
+
+function error(message) {
+    console.error(message); //eslint-disable-line no-console
+}
+
+function loadModule(path) {
+    require(path);
+}
 exports.log = log;
 exports.warn = warn;
 exports.error = error;
 exports.readFile = readFile;
 exports.loadModule = loadModule;
-exports.reset = () => {
+exports.reset = function reset() {
     exports.log = log;
     exports.warn = warn;
     exports.error = error;
